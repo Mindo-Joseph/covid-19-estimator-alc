4a5794f34a0b1fcd05b22dd202b7759498ea2305
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-04-11 14:36:06 +0300

    solve challenge 1

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,58 @@
 def estimator(data):
-  return data
+  #Get the CurrentlyInfected for severe and impact cases
+  currentInfectedImpact = CovidEstimator.currentlyInfectedImpact(data["reportedCases"])
+  currentInfectedSevere = CovidEstimator.currentlyInfectedSevere(data["reportedCases"])
+  # The infections by requested_time
+  infectionsByRequestedTimeImpact = CovidEstimator.infectionsByTime(data["periodType"],data["timeToElapse"],currentInfectedImpact)
+  infectionsByRequestedTimeSevere = CovidEstimator.infectionsByTime(data["periodType"],data["timeToElapse"],currentInfectedSevere)
+  output_data = {
+    "data":data,
+    "estimates": {
+      "impact": {
+            "currentlyInfected": currentInfectedImpact,
+            "infectionsByRequestedTime":infectionsByRequestedTimeImpact ,
+            "severeCasesByRequestedTime": ,
+            "hospitalBedsByRequestedTime": ,
+            "casesForICUByRequestedTime": ,
+            "casesForVentilatorsByRequestedTime": ,
+            "dollarsInFlight": 
+        },
+        "severe": {
+            "currentlyInfected": currentInfectedSevere,
+            "infectionsByRequestedTime":infectionsByRequestedTimeSevere ,
+            "severeCasesByRequestedTime": ,
+            "hospitalBedsByRequestedTime": ,
+            "casesForICUByRequestedTime": ,
+            "casesForVentilatorsByRequestedTime": ,
+            "dollarsInFlight": 
+        }
+    }
+  }
+  return output_data
+
+
+
+
+class CovidEstimator:
+  def currentlyInfectedImpact(reportedCases):
+    return reportedCases * 10
+  def currentlyInfectedSevere(reportedCases):
+    return reportedCases * 50
+  def infectionsByTime(period_type,period,currently_infected):
+    if period_type == "weeks":
+      weeks_to_days = int(period) * 7
+      factor = weeks_to_days // 3
+      infections = currently_infected * (2 ** factor)
+      return infections
+
+    elif period_type == "months":
+      months_to_days = int(period) * 30
+      factor = months_to_days // 3
+      infections = currently_infected * (2**factor)
+      return infections
+    
+    else:
+      factor = int(period) // 3
+      infections = currently_infected * (2 ** factor)
+      return infections
+
