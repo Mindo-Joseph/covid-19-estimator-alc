4cf5e30eae3e13ae12486fcbf7325c78a6521a5e
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-04-13 18:34:22 +0300

    change tabs to space

--- src/estimator.py
+++ src/estimator.py
@@ -2,93 +2,93 @@ def estimator(data):
 
     # Best case
 
-    currentlyInfectedImpact = Covid19Cases.currentlyInfectedImpact(
+    bestCase = Covid19Cases.estimateBestCase(
         data['reportedCases'])
 
     # Worst case
 
-    currentlyInfectedSevere = Covid19Cases.currentlyInfectedSevere(
+    worstCase = Covid19Cases.estimateWorstCase(
         data['reportedCases'])
 
     # Estimated best case after a specified time
 
-    infectionsByTimeImpact = Covid19Cases.infectionsByTime(
-        data["periodType"], data['timeToElapse'], currentlyInfectedImpact)
+    bestCase_byTime = Covid19Cases.estimateCasesByTime(
+        data["periodType"], data['timeToElapse'], bestCase)
 
     # Estimated worst case after a specified time
 
-    infectionsByTimeSevere = Covid19Cases.infectionsByTime(
-        data["periodType"], data['timeToElapse'], currentlyInfectedSevere)
+    worstCase_byTime = Covid19Cases.estimateCasesByTime(
+        data["periodType"], data['timeToElapse'], worstCase)
 
     # Estimated best case for severe cases after a period of time
 
-    casesByRequestedTimeImpact = Covid19Cases.estimatesevereCasesByRequestedTime(
-        infectionsByTimeImpact)
+    bestCase_severeCases = Covid19Cases.estimatesevereCasesByRequestedTime(
+        bestCase_byTime)
 
     # Estimated worst case for severe cases after a period of time
 
-    casesByRequestedTimeSevere = Covid19Cases.estimatesevereCasesByRequestedTime(
-        infectionsByTimeSevere)
+    worstCase_severeCases = Covid19Cases.estimatesevereCasesByRequestedTime(
+        worstCase_byTime)
 
     # Estimated best case available hospital beds for severe cases
 
     bestCase_avalilableBeds = Covid19Cases.estimateAvailableHospitalBeds(
-        data["totalHospitalBeds"], casesByRequestedTimeImpact)
+        data["totalHospitalBeds"], bestCase_severeCases)
 
     # Estimated worst case available hospital beds for severe cases
 
     worstCase_avalilableBeds = Covid19Cases.estimateAvailableHospitalBeds(
-        data["totalHospitalBeds"], casesByRequestedTimeSevere)
+        data["totalHospitalBeds"], worstCase_severeCases)
 
     # Estimated best case requiring ICU
 
     bestCase_requiringICU = Covid19Cases.estimateSevereCasesRequireIcu(
-        infectionsByTimeImpact)
+        bestCase_byTime)
 
     # Estimated worst case requiring ICU
 
     worstCase_requiringICU = Covid19Cases.estimateSevereCasesRequireIcu(
-        infectionsByTimeSevere)
+        worstCase_byTime)
 
     # Estimated best case requiring ventilators
 
     bestCase_Ventilators = Covid19Cases.estimateSevereCasesRequireVentilators(
-        infectionsByTimeImpact)
+        bestCase_byTime)
 
     # Estimated worst case requiring ventilators
 
     worstCase_Ventilators = Covid19Cases.estimateSevereCasesRequireVentilators(
-        infectionsByTimeSevere)
+        worstCase_byTime)
 
     # Estimated best case economic loss
 
     bestCase_loss = Covid19Cases.estimateEconomicLoss(
         data["periodType"], data['timeToElapse'],
         data["region"]["avgDailyIncomePopulation"],
-        data["region"]["avgDailyIncomeInUSD"], infectionsByTimeImpact)
+        data["region"]["avgDailyIncomeInUSD"], bestCase_byTime)
 
     # Estimated worst case economic loss
 
     worstCase_loss = Covid19Cases.estimateEconomicLoss(
         data["periodType"], data['timeToElapse'],
         data["region"]["avgDailyIncomePopulation"],
-        data["region"]["avgDailyIncomeInUSD"], infectionsByTimeSevere)
+        data["region"]["avgDailyIncomeInUSD"], worstCase_byTime)
 
     output_data = {
         "data": data,
         "impact": {
-            "currentlyInfected": currentlyInfectedImpact,
-            "infectionsByRequestedTime": infectionsByTimeImpact,
-            "severeCasesByRequestedTime": casesByRequestedTimeImpact,
+            "currentlyInfected": bestCase,
+            "infectionsByRequestedTime": bestCase_byTime,
+            "severeCasesByRequestedTime": bestCase_severeCases,
             "hospitalBedsByRequestedTime": bestCase_avalilableBeds,
             "casesForICUByRequestedTime": bestCase_requiringICU,
             "casesForVentilatorsByRequestedTime": bestCase_Ventilators,
             "dollarsInFlight": bestCase_loss
         },
         "severeImpact": {
-            "currentlyInfected": currentlyInfectedSevere,
-            "infectionsByRequestedTime": infectionsByTimeSevere,
-            "severeCasesByRequestedTime": casesByRequestedTimeSevere,
+            "currentlyInfected": worstCase,
+            "infectionsByRequestedTime": worstCase_byTime,
+            "severeCasesByRequestedTime": worstCase_severeCases,
             "hospitalBedsByRequestedTime": worstCase_avalilableBeds,
             "casesForICUByRequestedTime": worstCase_requiringICU,
             "casesForVentilatorsByRequestedTime": worstCase_Ventilators,
@@ -104,21 +104,21 @@ def estimator(data):
 class Covid19Cases():
 
     @staticmethod
-    def currentlyInfectedImpact(reported_cases):
+    def estimateBestCase(reported_cases):
 
         """Estimates the possibly infected people"""
 
         return float(reported_cases * 10)
 
     @staticmethod
-    def currentlyInfectedSevere(reported_cases):
+    def estimateWorstCase(reported_cases):
 
         """Estimates the severe possibility of infected people"""
 
         return float(reported_cases * 50)
 
     @staticmethod
-    def infectionsByTime(period_type, period, currently_infected):
+    def estimateCasesByTime(period_type, period, currently_infected):
 
         """Estimates the number of infections in specified days"""
 
@@ -143,12 +143,12 @@ class Covid19Cases():
 
             # calculates factor
             factor_inMonths = int(days_in_months / 3)
+
             infenctions_in_months = currently_infected * (2**factor_inMonths)
 
             return float(infenctions_in_months)
 
         else:
-
 
             # calculates the factor for the days
 
